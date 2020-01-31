181b8920c41680f4248f9506c769f94bafc3bcea
david-mcgrouther <dbmcgrouther@gmail.com>
2020-01-31 11:16:51 -0800

    added dog picture placeholder. also got camera to work so you can take your own picture by clicking a button

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # selfie-webapp
+
+used this heavily https://codecourse.com/watch/take-photos-with-your-webcam-in-your-browser
+
+used the link below to fix an issue
+https://stackoverflow.com/questions/53626318/chrome-update-failed-to-execute-createobjecturl-on-url
--- video.js
+++ video.js
@@ -1,5 +1,8 @@
 (function () {
     var video = document.getElementById('video'),
+        canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d'),
+        photo =  document.getElementById('photo'),
         vendorUrl = window.URL || window.webkitURL;
 
     navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
@@ -14,4 +17,10 @@
     }, function(error) {
 
     });
+
+    document.getElementById('capture').addEventListener('click', function() {
+        context.drawImage(video, 0, 0, 400, 300);
+        photo.setAttribute('src', canvas.toDataURL('image/png'));
+    });
+
 })();
